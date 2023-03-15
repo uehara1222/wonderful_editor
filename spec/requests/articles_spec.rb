5c802d48a6c75c9f4e0e462ea7ef8f7d04ac4711
j uehara <>
2023-03-16 01:49:36 +0900

    記事詳細のAPIとテストの実装

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "Api::V1::Articles", type: :request do
     let!(:article2) { create(:article, updated_at: 2.days.ago) }
     let!(:article3) { create(:article) }
 
-    fit "記事の一覧が取得できる" do
+    it "記事の一覧が取得できる" do
       subject
       res = JSON.parse(response.body)
 
@@ -17,6 +17,36 @@ RSpec.describe "Api::V1::Articles", type: :request do
       expect(res.map {|d| d["id"] }).to eq [article3.id, article1.id, article2.id]
       expect(res[0].keys).to eq ["id", "title", "updated_at", "user"]
       expect(res[0]["user"].keys).to eq ["id", "name", "email"]
+    end
+  end
+
+  describe "GET /article/:id" do
+    subject { get(api_v1_article_path(article_id)) }
+
+    context "指定したidの記事が存在する場合" do
+      let(:article) { create(:article) }
+      let(:article_id) { article.id }
+
+      it "記事の詳細が取得できる" do
+        subject
+        res = JSON.parse(response.body)
+
+        expect(response).to have_http_status(:ok)
+        expect(res["id"]).to eq article.id
+        expect(res["title"]).to eq article.title
+        expect(res["body"]).to eq article.body
+        expect(res["updated_at"]).to be_present
+        expect(res["user"]["id"]).to eq article.user.id
+        expect(res["user"].keys).to eq ["id", "name", "email"]
+      end
+    end
+
+    context "指定したidの記事が存在しない場合" do
+      let(:article_id) { 10000 }
+
+      it "記事が見つからずエラーを返す" do
+        expect { subject }.to raise_error ActiveRecord::RecordNotFound
+      end
     end
   end
 end
