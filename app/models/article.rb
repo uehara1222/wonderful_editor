a61e5bd6235701eca741220f20ef0bf5067ad064
j uehara <>
2023-03-26 18:23:40 +0900

    Article model に下書き機能を追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,20 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :article_likes, dependent: :destroy
 
-  validates :title, presence: true
+  validates :title, presence: true, if: :draft?
+  validates :title, :body, presence: true, if: :published?
+
+  enum status: { draft: 0, published: 1 }
+
+  def publish
+    update(status: :published)
+  end
+
+  def draft?
+    status == "draft"
+  end
+
+  def published?
+    status == "published"
+  end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,20 +1,30 @@
 require "rails_helper"
 
 RSpec.describe Article, type: :model do
-  context "タイトルがある場合" do
-    let!(:user) { create(:user) }
+  context "タイトルが入力されている場合" do
+    let(:article) { build(:article) }
 
-    it "記事を作れる" do
-      article = build(:article)
+    it "下書き状態の記事の作成ができる" do
       expect(article).to be_valid
+      expect(article.status).to eq "draft"
     end
   end
 
-  context "タイトルがない場合" do
-    let!(:user) { create(:user) }
+  context "タイトルはあるが本文がない場合" do
+    let(:article) { build(:article, body: nil) }
 
-    it "記事を作れずエラーを返す" do
-      article = build(:article, title: nil)
+    it "記事を公開状態では作成できない" do
+      article.publish
+      expect(article.status).to eq "published"
+      expect(article).to be_invalid
+      expect(article.errors.details[:body][0][:error]).to eq :blank
+    end
+  end
+
+  context "タイトルが入力されてない場合" do
+    let(:article) { build(:article, title: nil) }
+
+    it "記事の作成ができない" do
       expect(article).to be_invalid
       expect(article.errors.details[:title][0][:error]).to eq :blank
     end
